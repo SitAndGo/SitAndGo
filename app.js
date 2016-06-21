150ce5a66f31c57c38057406e00e3d53305242e3
yukkuri_sinai <yukkuri.main@gmail.com>
2016-06-21 20:36:00 +0900

    ルータの構成を変更

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const logger = require("morgan");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 
-const routes = require("./routes/index");
+const routes = require("./routes/routes");
 
 const app = express();
 
@@ -21,7 +21,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", routes);
+routes(app);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
---
+++ routes/routes.js
@@ -0,0 +1,5 @@
+const routes = (app) => {
+	app.use("/", require("./root/index"));
+};
+
+module.exports = routes;
